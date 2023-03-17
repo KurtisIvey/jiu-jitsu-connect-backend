42980384b3e9104be125d236e8c28247c13d94da
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-03-17 14:18:56 -0500

    add cors to prep for live testing or registration and login

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-require("./utilities/mongoConfig");
+const cors = require("cors");
+
 const port = process.env.PORT || 3001;
 
 // middlewares
 app.use(express.json());
+app.use(
+  cors({
+    // setting credentials true allows access-control header to true as well
+    // absolute requirement, otherwise cors issues will follow
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,DELETE",
+
+    origin: ["http://localhost:5173"],
+  })
+);
 
 // mongodb
 const mongoose = require("mongoose");
