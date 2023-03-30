a59392f55fc53454da832cba005c09a2355fb7b5
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-03-30 14:03:32 -0500

    update port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT;
 
 // middlewares
 app.use(express.json());
