6c1ba5184c06736629663bf18146a21b09360c28
KurtisIvey <kurtiveycodes@gmail.com>
2023-08-03 22:58:49 -0500

    update cors

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,DELETE",
 
-    origin: ["http://localhost:5173", "https://jiujitsu-connect.herokuapp.com"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://jiujitsu-connect.herokuapp.com"],
   })
 );
 
